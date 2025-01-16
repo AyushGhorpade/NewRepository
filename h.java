e6c097a94bf118ab508e1f07b0a8118fb70f5ad1
AyushGhorpade <ayushghorpade02@gmail.com>
2025-01-16 10:58:09 +0530

    Add files via upload

---
+++ h.java
@@ -0,0 +1,16 @@
+class h
+{
+    public static void main (String arg[])
+    {
+        int a=10;
+        int b=20;
+        if(a>b)
+        {
+            System.out.println("a is greter");
+        }
+        else
+        {
+            System.out.println("b is greter");
+        }
+    }
+}
